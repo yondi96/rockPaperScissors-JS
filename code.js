31ce10bcd4da223fd040b3d43643f91b5430ceb4
yondi96 <bautista.dioncarlo@gmail.com>
2021-11-17 18:05:21 +0800

    second commit

--- code.js
+++ code.js
@@ -15,22 +15,34 @@ $(".icon").click(function() {
     console.log("tie");
   } else if(userChoice == "rock" && compShot == "scissors") {
     console.log("You Won!");
+    userScore++;
   } else if(userChoice == "rock" && compShot == "paper") {
     console.log("You Lose");
+    enemyScore++;
   } else if(userChoice == "scissors" && compShot == "paper") {
     console.log("You win!");
+    userScore++;
   } else if(userChoice == "scissors" && compShot == "rock") {
     console.log("You lose.");
+    enemyScore++;
   } else if(userChoice == "paper" && compShot == "rock") {
     console.log("You win!");
+    userScore++;
   } else if(userChoice == "paper" && compShot == "scissors") {
     console.log("You lose.");
-  };
+    enemyScore++;
+  }
+
+  // this is wrong
+  if(userScore === 3 || enemyScore === 3) {
+    console.log("END GAME");
+  }
 })
 
 // generate random shot
 function randomShot() {
   let randomNum = Math.floor(Math.random()*3);
   compShot = shots[randomNum];
-  console.log(userChoice, compShot);
+  $(".user-score").html(userScore);
+  $(".comp-score").html(enemyScore);
 }
